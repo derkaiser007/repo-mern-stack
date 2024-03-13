5360c28989452e61ac07b46001d5568bd9cbdce1
derkaiser007 <nirajshrivastav01@gmail.com>
2024-03-13 21:48:34 +0530

    added mern stack ecommerce project with redux

--- project-1/frontend/src/App.jsx
+++ project-1/frontend/src/App.jsx
@@ -1,24 +1,34 @@
-import React, { useEffect, useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Layout/Header/Header";
-import Footer from './components/Layout/Footer/Footer'
-import Home from "./components/Home/Home";
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="app-container">
-      <Router>
-        <div className="box">
-          <Header />
-        </div>
-        <Home />
-        <div className="box">
-          <Footer />
-        </div>
-      </Router>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
